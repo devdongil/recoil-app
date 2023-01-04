12f9c2db8b9259d8c5f9ac5f1c68085e582eca12
dan <dan.choi@tosslab.com>
2023-01-04 13:50:25 +0900

    Refactor: Recoil 상태 객체 패키징
    recoilState 객체로 감싸서 한곳에서 관리하려는 목적

--- src/stage2/TodoList.js
+++ src/stage2/TodoList.js
@@ -1,72 +1,74 @@
 import React from 'react';
 import {
-  RecoilRoot,
   atom,
   selector,
-  useRecoilState,
   useRecoilValue,
 } from 'recoil';
+import TodoListStats from "./TodoListStats";
+import TodoListFilter from "./TodoListFilter";
 import TodoItemCreator from "./TodoItemCreator";
 import TodoItem from "./TodoItem";
-import TodoListFilter from "./TodoListFilter";
-import TodoListStats from "./TodoListStats";
 
-/**
- * atom은 Recoil의 상태를 표현한다<br>
- * https://recoiljs.org/ko/docs/api-reference/core/atom <br>
- * @type {RecoilState<*[]>}
- */
-const todoListState = atom({
-  key: 'todoListState',
-  default: [],
-});
+const recoilState = {
+  /**
+   * atom은 Recoil의 상태를 표현한다<br>
+   * https://recoiljs.org/ko/docs/api-reference/core/atom <br>
+   * @type {RecoilState<*[]>}
+   */
+  todoListState: atom({
+    key: 'todoListState',
+    default: [],
+  }),
+  todoListFilterState: atom({
+    key: 'todoListFilterState',
+    default: 'Show All', // Show All, Show Completed, Show Uncompleted
+  }),
+  /**
+   * selector는 Recoil에서 함수나 파생된 상태를 나타낸다.<br>
+   * https://recoiljs.org/ko/docs/api-reference/core/selector <br>
+   * @type {RecoilValueReadOnly<unknown>}
+   */
+  filteredTodoListState: selector({
+    key: 'filteredTodoListState',
+    get: ({ get }) => {
+      const filter = get(recoilState.todoListFilterState);
+      const list = get(recoilState.todoListState);
 
-const todoListFilterState = atom({
-  key: 'todoListFilterState',
-  default: 'Show All', // Show All, Show Completed, Show Uncompleted
-});
+      switch(filter) {
+        case "Show Completed":
+          return list.filter((todoItem) => todoItem.isComplete);
+        case "Show Uncompleted":
+          return list.filter((todoItem) => !todoItem.isComplete);
+        default:
+          return list;
+      }
+    },
+  }),
+  todoListStatsState: selector({
+    key: 'todoListStatsState',
+    get: ({ get }) => {
+      const todoList = get(recoilState.todoListState);
+      const totalNum = todoList.length;
+      const totalCompleteNum = todoList.filter((todoItem) => todoItem.isComplete).length;
+      const totalUncompleteNum = totalNum - totalCompleteNum;
+      const percenterComplete = totalNum === 0 ? 0 : Math.round(totalCompleteNum / totalNum * 100);
 
-/**
- * selector는 Recoil에서 함수나 파생된 상태를 나타낸다.<br>
- * https://recoiljs.org/ko/docs/api-reference/core/selector <br>
- * @type {RecoilValueReadOnly<unknown>}
- */
-const filteredTodoListState = selector({
-  key: 'filteredTodoListState',
-  get: ({ get }) => {
-    const filter = get(todoListFilterState);
-    const list = get(todoListState);
+      return {
+        totalNum,
+        totalCompleteNum,
+        totalUncompleteNum,
+        percenterComplete,
+      };
+    },
+  }),
+};
 
-    switch(filter) {
-      case "Show Completed":
-        return list.filter((todoItem) => todoItem.isComplete);
-      case "Show Uncompleted":
-        return list.filter((todoItem) => !todoItem.isComplete);
-      default:
-        return list;
-    }
-
-  },
-});
-
-const todoListStatsState = selector({
-  key: 'todoListStatsState',
-  get: ({ get }) => {
-    const todoList = get(todoListState);
-    const totalNum = todoList.length;
-    const totalCompleteNum = todoList.filter((todoItem) => todoItem.isComplete).length;
-    const totalUncompleteNum = totalNum - totalCompleteNum;
-    const percenterComplete = totalNum === 0 ? 0 : Math.round(totalCompleteNum / totalNum * 100);
-
-    return {
-      totalNum,
-      totalCompleteNum,
-      totalUncompleteNum,
-      percenterComplete,
-    };
-  },
-
-});
+const {
+  filteredTodoListState,
+  todoListStatsState,
+  todoListFilterState,
+  todoListState,
+} = recoilState;
 
 function TodoItemList() {
   /**
