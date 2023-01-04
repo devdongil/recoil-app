9a59eee24b344595fa3155622117e8bd937aa5b3
dan <dan.choi@tosslab.com>
2023-01-04 13:13:38 +0900

    Add: 할일 필터
    필터 기능 추가

--- src/stage2/TodoList.js
+++ src/stage2/TodoList.js
@@ -8,6 +8,7 @@ import {
 } from 'recoil';
 import TodoItemCreator from "./TodoItemCreator";
 import TodoItem from "./TodoItem";
+import TodoListFilter from "./TodoListFilter";
 
 /**
  * atom은 Recoil의 상태를 표현한다<br>
@@ -19,12 +20,40 @@ const todoListState = atom({
   default: [],
 });
 
+const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All', // Show All, Show Completed, Show Uncompleted
+});
+
+/**
+ * selector는 Recoil에서 함수나 파생된 상태를 나타낸다.<br>
+ * https://recoiljs.org/ko/docs/api-reference/core/selector <br>
+ * @type {RecoilValueReadOnly<unknown>}
+ */
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch(filter) {
+      case "Show Completed":
+        return list.filter((todoItem) => todoItem.isComplete);
+      case "Show Uncompleted":
+        return list.filter((todoItem) => !todoItem.isComplete);
+      default:
+        return list;
+    }
+
+  },
+});
+
 function TodoItemList() {
   /**
    * useRecoilValue는 atom을 읽기만 할 때 이 Hook를 사용한다<br>
    * https://recoiljs.org/ko/docs/api-reference/core/useRecoilValue <br>
    */
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState); // todoListState
 
   return (<>{
     todoList.map((todoItem) =>
@@ -40,7 +69,8 @@ function TodoItemList() {
 function TodoList() {
   return (
     <div>
-      <TodoItemCreator {...{todoListState}} />
+      <TodoListFilter {...{ todoListFilterState }} />
+      <TodoItemCreator {...{ todoListState }} />
       <TodoItemList />
     </div>
   );
---
+++ src/stage2/TodoListFilter.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+import TodoItemCreator from "./TodoItemCreator";
+
+function TodoListFilter({ todoListFilterState }) {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+  const updateFilter = ({ target: { value }}) => setFilter(value);
+
+  return (<>
+    Filter:
+    <select value={filter} onChange={updateFilter}>
+      <option value="Show All">All</option>
+      <option value="Show Completed">Completed</option>
+      <option value="Show Uncompleted">Uncompleted</option>
+    </select>
+  </>);
+}
+
+export default TodoListFilter;
